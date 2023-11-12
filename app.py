0a31dc67275d550263b60883bf199b45abad0d4a
Ricardo André <114098654+ricardoandreom@users.noreply.github.com>
2023-11-12 12:04:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # libraries
-import pandas as pd
 import streamlit as st
 from pathlib import Path
 import app_functions as app
@@ -14,18 +13,12 @@ black_logo_url = "https://raw.githubusercontent.com/ricardoandreom/Data/master/I
 
 # LANDING PAGE LINK
 SOCIAL_MEDIA = {
-    "Portfolio/Landing page": "https://ricardo-marques-digital-cv.streamlit.app/"
+    "Portfolio/Landing page": "https://ricardoandreom.github.io/ricardo_portfolio_page/"
 }
 
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-
-############################################################################################
-
-# creating the df
-df = app.get_df_complete()
-df_orig = df.copy()
 
 ############################################################################################
 
@@ -36,64 +29,73 @@ st.title('Player similarities in Top-5 Leagues ⚽📈')
 
 st.write('#')
 # MAIN LOGO (WHITE)
-st.sidebar.image(white_logo_url, use_column_width=True)
-
-st.sidebar.markdown("""
-    <p style='font-size: 26px; font-weight: bold; color: black;'>Player similarity inputs:</p>
-""", unsafe_allow_html=True)
-
-st.sidebar.markdown("""
-    <p style='font-size: 16px; font-weight: bold; color: black;'>Assess a player level of similarity of with players in the same position in Top-5 Leagues:</p>
-""", unsafe_allow_html=True)
-
-# choosing the player that the user wants to see similar players
-player = st.sidebar.selectbox("**Name of the player:**", df_orig['Player'])
-
-# GK note missing data
-st.sidebar.write('Note: No data for goalkeepers yet!')
-
-############################################################################################
-# SIMILARITIES PART
-
-player_position_sim = df_orig.set_index('Player').loc[player]['Position']
-
-# define the max value that the user can choose for min 90s played
-max_value_sim = int(df['90s'].max()) # - 5)
-
-min_ninety_sim = st.sidebar.slider("**Select mininum 90s played you want to consider for the model similarities:**", 1, max_value_sim, key="min_ninety_sim")
-
-df_orig = df_orig[(df_orig['Position'] == player_position_sim) & (df_orig['90s'] >= min_ninety_sim)]
-
-player_similars = app.similarity_process(df_orig,player)
-
-league_sim = st.sidebar.selectbox("**League of the players:**", ['La Liga', 'Bundesliga', 'Premier League', 'Serie A', 'Ligue 1', 'All'])
-age_sim = st.sidebar.selectbox("**Max age of the players:**", player_similars['Age'])
-
-
-if league_sim != 'All':
-    player_similars = player_similars[(player_similars['League'] == league_sim) & (player_similars['Age'] <= age_sim)
-    ].head(15)
-else:
-    player_similars = player_similars[
-        player_similars['Age'] <= age_sim].head(15)
-
-#########################################################################################################################
+twitter_url = 'https://twitter.com/HspaceAnalytics'
+# MAIN LOGO (WHITE)
+st.sidebar.markdown(f'<a href="{twitter_url}" target="_blank"><img src="{white_logo_url}" alt="Logo" style="width: 100%;"></a>', unsafe_allow_html=True)
 
 # rest of the streamlit part
 st.markdown("""
-    <p style='font-size: 19px; font-weight: bold; color: white;'> 🔴 Choose a player and find out who are the 15 most
+    <p style='font-size: 19px; font-weight: bold; color: black;'> 🔴 Choose a player and find out who are the 15 most
     similar players, according to the implicit model. You can filter the results per age, league and mininum 90s played.</p>
 """, unsafe_allow_html=True)
 
-# fazer display da player_similars
-st.title('Who are the most similar players to ' + player + '?')
-st.dataframe(player_similars.set_index('Player'))
-
-#####################################################
+# GK note missing data
+st.sidebar.write('Note: No data for goalkeepers yet!')
 
 # portfolio url
 st.sidebar.write("#")
 cols = st.sidebar.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
     cols[index].write(f"**Made by @ricardoandreom 🚀⚽\n[{platform}]({link})**")
+
+st.sidebar.markdown("""
+    <p style='font-size: 26px; font-weight: bold; color: black;'>Inputs:</p>
+""", unsafe_allow_html=True)
+
+# creating the df
+df = app.get_df_complete().reset_index()
+max_value_sim = int(df['90s'].max())
+min_ninety_sim = round(max_value_sim / 2)
+df = df[(df['90s'] >= min_ninety_sim)]
+df_orig = df.copy()
+
+# choosing the player that the user wants to see similar players
+player = st.sidebar.selectbox("**Name of the player:**", df_orig['Player'])
+# fazer display da player_similars
+st.title('Who are the most similar players to ' + player + '?')
+
+############################################################################################
+# SIMILARITIES PART
+
+league_sim_options = ['La Liga', 'Bundesliga', 'Premier League', 'Serie A', 'Ligue 1', 'All']
+
+default_league_index = league_sim_options.index('All')
+
+league_sim = st.sidebar.selectbox("**League of the players:**", league_sim_options, index=default_league_index)
+
+age_sim = st.sidebar.slider("**Max age of the players:**", 16, 40, value=38)
+
+kofi_link = "https://ko-fi.com/ricardoandreom"
+st.sidebar.markdown(f'<a href="{kofi_link}" target="_blank"><img src="https://cdn.ko-fi.com/cdn/kofi1.png" alt="Buy Me a Coffee" style="height: 40px;"></a>', unsafe_allow_html=True)
+try:
+    player_similars = app.similarity_process_v2(df_orig,player)[['Player',
+                                                      '% similarity',
+                                                      'Squad',
+                                                      'Position',
+                                                      'Secondary Position'
+                                                      'Age',
+                                                      '90s'
+                                                      'League']]
+    player_similars = player_similars[
+        (player_similars['Age'] <= age_sim) &
+        (player_similars['League'] == league_sim) if league_sim != 'All' else (player_similars['Age'] <= age_sim)].head(
+        15)
+
+    st.dataframe(player_similars.set_index('Player'))
+except KeyError:
+    st.warning("Error: We have no similarities for this player because this players has no enough minutes to get conclusions.")
+
+
+
+
 
